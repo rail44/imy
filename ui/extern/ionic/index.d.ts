cff061de2e1844ab3da24bfe4a54375a67264b52
Satoshi Amemiya <amemiya@protonmail.com>
2020-07-01 20:22:27 +0900

    ionic

---
+++ ui/extern/ionic/index.d.ts
@@ -0,0 +1,539 @@
+/* Ionic custom elements bundle */
+
+import { Components } from "../types/interface";
+
+interface IonActionSheet extends Components.IonActionSheet, HTMLElement {}
+export const IonActionSheet: {
+  prototype: IonActionSheet;
+  new (): IonActionSheet;
+};
+
+interface IonAlert extends Components.IonAlert, HTMLElement {}
+export const IonAlert: {
+  prototype: IonAlert;
+  new (): IonAlert;
+};
+
+interface IonApp extends Components.IonApp, HTMLElement {}
+export const IonApp: {
+  prototype: IonApp;
+  new (): IonApp;
+};
+
+interface IonAvatar extends Components.IonAvatar, HTMLElement {}
+export const IonAvatar: {
+  prototype: IonAvatar;
+  new (): IonAvatar;
+};
+
+interface IonBackButton extends Components.IonBackButton, HTMLElement {}
+export const IonBackButton: {
+  prototype: IonBackButton;
+  new (): IonBackButton;
+};
+
+interface IonBackdrop extends Components.IonBackdrop, HTMLElement {}
+export const IonBackdrop: {
+  prototype: IonBackdrop;
+  new (): IonBackdrop;
+};
+
+interface IonBadge extends Components.IonBadge, HTMLElement {}
+export const IonBadge: {
+  prototype: IonBadge;
+  new (): IonBadge;
+};
+
+interface IonButton extends Components.IonButton, HTMLElement {}
+export const IonButton: {
+  prototype: IonButton;
+  new (): IonButton;
+};
+
+interface IonButtons extends Components.IonButtons, HTMLElement {}
+export const IonButtons: {
+  prototype: IonButtons;
+  new (): IonButtons;
+};
+
+interface IonCard extends Components.IonCard, HTMLElement {}
+export const IonCard: {
+  prototype: IonCard;
+  new (): IonCard;
+};
+
+interface IonCardContent extends Components.IonCardContent, HTMLElement {}
+export const IonCardContent: {
+  prototype: IonCardContent;
+  new (): IonCardContent;
+};
+
+interface IonCardHeader extends Components.IonCardHeader, HTMLElement {}
+export const IonCardHeader: {
+  prototype: IonCardHeader;
+  new (): IonCardHeader;
+};
+
+interface IonCardSubtitle extends Components.IonCardSubtitle, HTMLElement {}
+export const IonCardSubtitle: {
+  prototype: IonCardSubtitle;
+  new (): IonCardSubtitle;
+};
+
+interface IonCardTitle extends Components.IonCardTitle, HTMLElement {}
+export const IonCardTitle: {
+  prototype: IonCardTitle;
+  new (): IonCardTitle;
+};
+
+interface IonCheckbox extends Components.IonCheckbox, HTMLElement {}
+export const IonCheckbox: {
+  prototype: IonCheckbox;
+  new (): IonCheckbox;
+};
+
+interface IonChip extends Components.IonChip, HTMLElement {}
+export const IonChip: {
+  prototype: IonChip;
+  new (): IonChip;
+};
+
+interface IonCol extends Components.IonCol, HTMLElement {}
+export const IonCol: {
+  prototype: IonCol;
+  new (): IonCol;
+};
+
+interface IonContent extends Components.IonContent, HTMLElement {}
+export const IonContent: {
+  prototype: IonContent;
+  new (): IonContent;
+};
+
+interface IonDatetime extends Components.IonDatetime, HTMLElement {}
+export const IonDatetime: {
+  prototype: IonDatetime;
+  new (): IonDatetime;
+};
+
+interface IonFab extends Components.IonFab, HTMLElement {}
+export const IonFab: {
+  prototype: IonFab;
+  new (): IonFab;
+};
+
+interface IonFabButton extends Components.IonFabButton, HTMLElement {}
+export const IonFabButton: {
+  prototype: IonFabButton;
+  new (): IonFabButton;
+};
+
+interface IonFabList extends Components.IonFabList, HTMLElement {}
+export const IonFabList: {
+  prototype: IonFabList;
+  new (): IonFabList;
+};
+
+interface IonFooter extends Components.IonFooter, HTMLElement {}
+export const IonFooter: {
+  prototype: IonFooter;
+  new (): IonFooter;
+};
+
+interface IonGrid extends Components.IonGrid, HTMLElement {}
+export const IonGrid: {
+  prototype: IonGrid;
+  new (): IonGrid;
+};
+
+interface IonHeader extends Components.IonHeader, HTMLElement {}
+export const IonHeader: {
+  prototype: IonHeader;
+  new (): IonHeader;
+};
+
+interface IonImg extends Components.IonImg, HTMLElement {}
+export const IonImg: {
+  prototype: IonImg;
+  new (): IonImg;
+};
+
+interface IonInfiniteScroll extends Components.IonInfiniteScroll, HTMLElement {}
+export const IonInfiniteScroll: {
+  prototype: IonInfiniteScroll;
+  new (): IonInfiniteScroll;
+};
+
+interface IonInfiniteScrollContent extends Components.IonInfiniteScrollContent, HTMLElement {}
+export const IonInfiniteScrollContent: {
+  prototype: IonInfiniteScrollContent;
+  new (): IonInfiniteScrollContent;
+};
+
+interface IonInput extends Components.IonInput, HTMLElement {}
+export const IonInput: {
+  prototype: IonInput;
+  new (): IonInput;
+};
+
+interface IonItem extends Components.IonItem, HTMLElement {}
+export const IonItem: {
+  prototype: IonItem;
+  new (): IonItem;
+};
+
+interface IonItemDivider extends Components.IonItemDivider, HTMLElement {}
+export const IonItemDivider: {
+  prototype: IonItemDivider;
+  new (): IonItemDivider;
+};
+
+interface IonItemGroup extends Components.IonItemGroup, HTMLElement {}
+export const IonItemGroup: {
+  prototype: IonItemGroup;
+  new (): IonItemGroup;
+};
+
+interface IonItemOption extends Components.IonItemOption, HTMLElement {}
+export const IonItemOption: {
+  prototype: IonItemOption;
+  new (): IonItemOption;
+};
+
+interface IonItemOptions extends Components.IonItemOptions, HTMLElement {}
+export const IonItemOptions: {
+  prototype: IonItemOptions;
+  new (): IonItemOptions;
+};
+
+interface IonItemSliding extends Components.IonItemSliding, HTMLElement {}
+export const IonItemSliding: {
+  prototype: IonItemSliding;
+  new (): IonItemSliding;
+};
+
+interface IonLabel extends Components.IonLabel, HTMLElement {}
+export const IonLabel: {
+  prototype: IonLabel;
+  new (): IonLabel;
+};
+
+interface IonList extends Components.IonList, HTMLElement {}
+export const IonList: {
+  prototype: IonList;
+  new (): IonList;
+};
+
+interface IonListHeader extends Components.IonListHeader, HTMLElement {}
+export const IonListHeader: {
+  prototype: IonListHeader;
+  new (): IonListHeader;
+};
+
+interface IonLoading extends Components.IonLoading, HTMLElement {}
+export const IonLoading: {
+  prototype: IonLoading;
+  new (): IonLoading;
+};
+
+interface IonMenu extends Components.IonMenu, HTMLElement {}
+export const IonMenu: {
+  prototype: IonMenu;
+  new (): IonMenu;
+};
+
+interface IonMenuButton extends Components.IonMenuButton, HTMLElement {}
+export const IonMenuButton: {
+  prototype: IonMenuButton;
+  new (): IonMenuButton;
+};
+
+interface IonMenuToggle extends Components.IonMenuToggle, HTMLElement {}
+export const IonMenuToggle: {
+  prototype: IonMenuToggle;
+  new (): IonMenuToggle;
+};
+
+interface IonModal extends Components.IonModal, HTMLElement {}
+export const IonModal: {
+  prototype: IonModal;
+  new (): IonModal;
+};
+
+interface IonNav extends Components.IonNav, HTMLElement {}
+export const IonNav: {
+  prototype: IonNav;
+  new (): IonNav;
+};
+
+interface IonNavLink extends Components.IonNavLink, HTMLElement {}
+export const IonNavLink: {
+  prototype: IonNavLink;
+  new (): IonNavLink;
+};
+
+interface IonNote extends Components.IonNote, HTMLElement {}
+export const IonNote: {
+  prototype: IonNote;
+  new (): IonNote;
+};
+
+interface IonPicker extends Components.IonPicker, HTMLElement {}
+export const IonPicker: {
+  prototype: IonPicker;
+  new (): IonPicker;
+};
+
+interface IonPickerColumn extends Components.IonPickerColumn, HTMLElement {}
+export const IonPickerColumn: {
+  prototype: IonPickerColumn;
+  new (): IonPickerColumn;
+};
+
+interface IonPopover extends Components.IonPopover, HTMLElement {}
+export const IonPopover: {
+  prototype: IonPopover;
+  new (): IonPopover;
+};
+
+interface IonProgressBar extends Components.IonProgressBar, HTMLElement {}
+export const IonProgressBar: {
+  prototype: IonProgressBar;
+  new (): IonProgressBar;
+};
+
+interface IonRadio extends Components.IonRadio, HTMLElement {}
+export const IonRadio: {
+  prototype: IonRadio;
+  new (): IonRadio;
+};
+
+interface IonRadioGroup extends Components.IonRadioGroup, HTMLElement {}
+export const IonRadioGroup: {
+  prototype: IonRadioGroup;
+  new (): IonRadioGroup;
+};
+
+interface IonRange extends Components.IonRange, HTMLElement {}
+export const IonRange: {
+  prototype: IonRange;
+  new (): IonRange;
+};
+
+interface IonRefresher extends Components.IonRefresher, HTMLElement {}
+export const IonRefresher: {
+  prototype: IonRefresher;
+  new (): IonRefresher;
+};
+
+interface IonRefresherContent extends Components.IonRefresherContent, HTMLElement {}
+export const IonRefresherContent: {
+  prototype: IonRefresherContent;
+  new (): IonRefresherContent;
+};
+
+interface IonReorder extends Components.IonReorder, HTMLElement {}
+export const IonReorder: {
+  prototype: IonReorder;
+  new (): IonReorder;
+};
+
+interface IonReorderGroup extends Components.IonReorderGroup, HTMLElement {}
+export const IonReorderGroup: {
+  prototype: IonReorderGroup;
+  new (): IonReorderGroup;
+};
+
+interface IonRippleEffect extends Components.IonRippleEffect, HTMLElement {}
+export const IonRippleEffect: {
+  prototype: IonRippleEffect;
+  new (): IonRippleEffect;
+};
+
+interface IonRoute extends Components.IonRoute, HTMLElement {}
+export const IonRoute: {
+  prototype: IonRoute;
+  new (): IonRoute;
+};
+
+interface IonRouteRedirect extends Components.IonRouteRedirect, HTMLElement {}
+export const IonRouteRedirect: {
+  prototype: IonRouteRedirect;
+  new (): IonRouteRedirect;
+};
+
+interface IonRouter extends Components.IonRouter, HTMLElement {}
+export const IonRouter: {
+  prototype: IonRouter;
+  new (): IonRouter;
+};
+
+interface IonRouterLink extends Components.IonRouterLink, HTMLElement {}
+export const IonRouterLink: {
+  prototype: IonRouterLink;
+  new (): IonRouterLink;
+};
+
+interface IonRouterOutlet extends Components.IonRouterOutlet, HTMLElement {}
+export const IonRouterOutlet: {
+  prototype: IonRouterOutlet;
+  new (): IonRouterOutlet;
+};
+
+interface IonRow extends Components.IonRow, HTMLElement {}
+export const IonRow: {
+  prototype: IonRow;
+  new (): IonRow;
+};
+
+interface IonSearchbar extends Components.IonSearchbar, HTMLElement {}
+export const IonSearchbar: {
+  prototype: IonSearchbar;
+  new (): IonSearchbar;
+};
+
+interface IonSegment extends Components.IonSegment, HTMLElement {}
+export const IonSegment: {
+  prototype: IonSegment;
+  new (): IonSegment;
+};
+
+interface IonSegmentButton extends Components.IonSegmentButton, HTMLElement {}
+export const IonSegmentButton: {
+  prototype: IonSegmentButton;
+  new (): IonSegmentButton;
+};
+
+interface IonSelect extends Components.IonSelect, HTMLElement {}
+export const IonSelect: {
+  prototype: IonSelect;
+  new (): IonSelect;
+};
+
+interface IonSelectOption extends Components.IonSelectOption, HTMLElement {}
+export const IonSelectOption: {
+  prototype: IonSelectOption;
+  new (): IonSelectOption;
+};
+
+interface IonSelectPopover extends Components.IonSelectPopover, HTMLElement {}
+export const IonSelectPopover: {
+  prototype: IonSelectPopover;
+  new (): IonSelectPopover;
+};
+
+interface IonSkeletonText extends Components.IonSkeletonText, HTMLElement {}
+export const IonSkeletonText: {
+  prototype: IonSkeletonText;
+  new (): IonSkeletonText;
+};
+
+interface IonSlide extends Components.IonSlide, HTMLElement {}
+export const IonSlide: {
+  prototype: IonSlide;
+  new (): IonSlide;
+};
+
+interface IonSlides extends Components.IonSlides, HTMLElement {}
+export const IonSlides: {
+  prototype: IonSlides;
+  new (): IonSlides;
+};
+
+interface IonSpinner extends Components.IonSpinner, HTMLElement {}
+export const IonSpinner: {
+  prototype: IonSpinner;
+  new (): IonSpinner;
+};
+
+interface IonSplitPane extends Components.IonSplitPane, HTMLElement {}
+export const IonSplitPane: {
+  prototype: IonSplitPane;
+  new (): IonSplitPane;
+};
+
+interface IonTab extends Components.IonTab, HTMLElement {}
+export const IonTab: {
+  prototype: IonTab;
+  new (): IonTab;
+};
+
+interface IonTabBar extends Components.IonTabBar, HTMLElement {}
+export const IonTabBar: {
+  prototype: IonTabBar;
+  new (): IonTabBar;
+};
+
+interface IonTabButton extends Components.IonTabButton, HTMLElement {}
+export const IonTabButton: {
+  prototype: IonTabButton;
+  new (): IonTabButton;
+};
+
+interface IonTabs extends Components.IonTabs, HTMLElement {}
+export const IonTabs: {
+  prototype: IonTabs;
+  new (): IonTabs;
+};
+
+interface IonText extends Components.IonText, HTMLElement {}
+export const IonText: {
+  prototype: IonText;
+  new (): IonText;
+};
+
+interface IonTextarea extends Components.IonTextarea, HTMLElement {}
+export const IonTextarea: {
+  prototype: IonTextarea;
+  new (): IonTextarea;
+};
+
+interface IonThumbnail extends Components.IonThumbnail, HTMLElement {}
+export const IonThumbnail: {
+  prototype: IonThumbnail;
+  new (): IonThumbnail;
+};
+
+interface IonTitle extends Components.IonTitle, HTMLElement {}
+export const IonTitle: {
+  prototype: IonTitle;
+  new (): IonTitle;
+};
+
+interface IonToast extends Components.IonToast, HTMLElement {}
+export const IonToast: {
+  prototype: IonToast;
+  new (): IonToast;
+};
+
+interface IonToggle extends Components.IonToggle, HTMLElement {}
+export const IonToggle: {
+  prototype: IonToggle;
+  new (): IonToggle;
+};
+
+interface IonToolbar extends Components.IonToolbar, HTMLElement {}
+export const IonToolbar: {
+  prototype: IonToolbar;
+  new (): IonToolbar;
+};
+
+interface IonVirtualScroll extends Components.IonVirtualScroll, HTMLElement {}
+export const IonVirtualScroll: {
+  prototype: IonVirtualScroll;
+  new (): IonVirtualScroll;
+};
+
+/**
+ * Utility to define all custom elements within this package using the tag name provided in the component's source. 
+ * When defining each custom element, it will also check it's safe to define by:
+ *
+ * 1. Ensuring the "customElements" registry is available in the global context (window).
+ * 2. The component tag name is not already defined.
+ *
+ * Use the standard [customElements.define()](https://developer.mozilla.org/en-US/docs/Web/API/CustomElementRegistry/define) 
+ * method instead to define custom elements individually, or to provide a different tag name.
+ */
+export declare const defineCustomElements: (opts?: any) => void;
+
+export * from '../types';
